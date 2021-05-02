2390ee4b626f22ed722c850016c4be189f2abc58
Kyuubang <bayhaqisptr04@gmail.com>
2021-05-02 15:42:03 +0700

    added semicolon

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 	public static void main (String [] args) {
 		System.out.println("Hello World again");
 		System.out.println("Hai!");
-		System.out.println("testing another build")
+		System.out.println("testing another build");
 				      
 	}
 	
